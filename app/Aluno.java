ecfdd5206f566428a102a93f6d4f261429cb9bb5
felipemacedo6785-ui <felipe.macedo6785@gmail.com>
2026-01-14 23:40:04 -0300

    Aluno.java

--- app/Aluno.java
+++ app/Aluno.java
@@ -1,5 +1,24 @@
 package app;
 
-public class Aluno {
+import base.Pessoa;
 
+public class Aluno extends Pessoa {
+
+    private String curso;
+    private int matricula;
+
+    public Aluno(String nome, int idade, String cpf, String curso, int matricula) {
+        super(nome, idade, cpf); // chama o construtor da superclasse
+        this.curso = curso;
+        this.matricula = matricula;
+    }
+
+    public void apresentar() {
+        System.out.println(
+            "Meu nome é " + nome +
+            ", tenho " + idade + " anos, CPF " + getCpf() +
+            ", curso " + curso +
+            " e matrícula " + matricula
+        );
+    }
 }
