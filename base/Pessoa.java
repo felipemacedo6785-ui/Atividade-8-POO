45be00b6f6c68f0b06db16e92f80271bb13484ed
felipemacedo6785-ui <felipe.macedo6785@gmail.com>
2026-01-14 23:39:04 -0300

    Update Pessoa.java

--- base/Pessoa.java
+++ base/Pessoa.java
@@ -2,4 +2,23 @@ package base;
 
 public class Pessoa {
 
+    public String nome;
+    protected int idade;
+    private String cpf;
+
+    // construtor vazio
+    public Pessoa() {
+    }
+
+    // construtor com todos os atributos
+    public Pessoa(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    // método pra acessar o private (porque filho não toca direto 👀)
+    public String getCpf() {
+        return cpf;
+    }
 }
