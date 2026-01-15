6b4f75c7338865fc0ae568288eba28280281aad3
felipemacedo6785-ui <felipe.macedo6785@gmail.com>
2026-01-14 23:40:35 -0300

    Main.java

--- app/Main.java
+++ app/Main.java
@@ -1,5 +1,16 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Aluno a1 = new Aluno(
+            "Felipe",
+            16,
+            "123.456.789-00",
+            "Informática",
+            2026
+        );
+
+        a1.apresentar();
+    }
 }
